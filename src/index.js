6437ffe0bd000cc57b3e12a69745ee4a405e6e72
pduchesne <pduchesne@gmail.com>
2018-12-14 19:26:18 +0100

    First react app skeleton

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Header = () => {
+  return <h2>Spotify Console</h2>;
+};
+
+ReactDOM.render(<Header />, document.getElementById("index"));
